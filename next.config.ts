b1e9265b3cabe5f935bcf7f08b7f8e7b02ba4b00
Vexco <xmantellato@gmail.com>
2026-09-21 21:03:25 +0200

    update next-config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Optional: set basePath if deploying to a specific repository path
+  // basePath: '/your-repo-name',
+
 };
 
 export default nextConfig;
