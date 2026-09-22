4f5aa8be04305b6421b25b6ef28642bdeb3f3464
Vexco <xmantellato@gmail.com>
2026-09-22 13:23:44 +0200

    try update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
@@ -7,8 +9,8 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   // Optional: set basePath if deploying to a specific repository path
-  // basePath: '/your-repo-name',
-
+  // basePath: '/xm-portfolio',
+  basePath: isProd ? '/xm-portfolio' : ''
 };
 
 export default nextConfig;
